c22d61bd44589f9d74127f78e1380d4c755d48d4
Jhonatan Vinicius <63015650+jhow-72@users.noreply.github.com>
2021-01-28 14:17:03 -0300

    Add files via upload

---
+++ Lista1/Exer2.java
@@ -0,0 +1,5 @@
+class Exer2{
+    public static void main(String[] args){
+        System.out.println("Estou\ntestando\no\nprintln()");
+    }
+}
---
+++ Lista1/Exer4.java
@@ -0,0 +1,6 @@
+class Exer4{
+    public static void main(String[] args){
+        System.out.println("6*(2+3)/(4+5) = " + (6*(2+3)/(4+5)) + " (somente a parte inteira)");
+        System.out.println("(42/7)*5+6*3+4 = "+((42/7)*5+6*3+4));
+    }
+}
