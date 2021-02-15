83afa86847a498fa24e2eab7f5a1f37075d2ca4d
Jhow_72 <jhow_72@usp.br>
2021-02-15 20:48:31 -0300

    commit 2/15/2021

---
+++ Lista3/ex11.java
@@ -0,0 +1,8 @@
+public class ex11{
+    public static void main(String[] args){
+        int soma = 0;
+        for(int k=4; k<=9; k++)
+            soma += k;
+        System.out.println(soma);
+    }
+}
