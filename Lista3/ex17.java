1d9f8b59bf30d554ec72e37a758f56d8e17de2f7
Jhow_72 <jhow_72@usp.br>
2021-02-18 14:55:53 -0300

    commit 2/18/2021

---
+++ Lista3/ex17.java
@@ -0,0 +1,37 @@
+/*
+    Recebe um numero do usúaria
+    passa como parâmetro 
+    e verifica se é primo ou não
+*/
+
+import java.util.*;
+
+public class ex17{
+    public static void main(String[] args){
+        Scanner key = new Scanner(System.in);
+        
+        System.out.print("Enter a number: ");
+        int num = key.nextInt();
+
+        check(num);
+    }
+
+    public static void check(int n){
+        boolean check = true;
+        if(n==0 || n==1)
+            check = false;
+        for(int i=2; i<=(n/2); i++){
+            if(n%i==0){
+                check = false;
+                break;
+            }
+        }
+        if(n==2)
+            check = true;
+
+        if(!check)
+            System.out.println(n+" Isn't a prime number.");
+        else 
+            System.out.println(n+" Is a prime number.");
+    }
+}
